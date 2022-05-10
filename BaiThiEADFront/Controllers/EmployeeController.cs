0c67156f61d3450acdb34e09eebc082210a34c2e
DESKTOP-70JFRMR\phuon <phuonghd@newit.co.jp>
2022-05-10 22:19:05 +0700

    front

---
+++ BaiThiEADFront/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using BaiThiEADFront.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiThiEADFront.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private Service1Client service = new Service1Client();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var data = service.GetList(null);
+            return View(data);
+        }
+
+        public ActionResult IndexAjax()
+        {
+            var searchString = this.Request.QueryString["searchString"];
+            var result = service.GetList(searchString != null ? searchString : null);
+            return PartialView("IndexAjax", result);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(Employee employee)
+        {
+            var result = service.CreateEmployee(employee);
+            if(!result)
+            {
+                return View("Error");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BaiThiEADFront/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using BaiThiEADFront.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiThiEADFront.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            Service1Client service = new Service1Client();
+            ViewBag.Message = $"Your application description page. {service.GetData(1)}";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
